ee255c6f7b4ea0980523eb8e4419eec44a10d409
noornawaz1 <97706855+noornawaz1@users.noreply.github.com>
2023-02-10 11:01:22 -0700

    Update DataUtilitiesTest.java

--- DataUtilitiesTest.java
+++ DataUtilitiesTest.java
@@ -15,9 +15,9 @@ import org.junit.*;
 public class DataUtilitiesTest {
 	
 	//START TEST CASES FOR METHOD 1 - calculateColumnTotal
-	//testing using mocking
+	//testing that the correct value is returned using positive numbers
 	@Test
-	public void calculateColumnTotalFiveValues() {
+	public void test1ColumnTotal() {
 	//setup for mocking 
 	    Mockery mockingContext = new Mockery();
 	    final Values2D values = mockingContext.mock(Values2D.class);
@@ -38,15 +38,67 @@ public class DataUtilitiesTest {
 	     });
 	     //testing the result we get with the expected result
 	     double result = DataUtilities.calculateColumnTotal(values, 0);
-	     assertEquals(result, 23.5, 0.000000001d);
- 	}    
-	//Testing using table that has null value
- 	@Test (expected = InvalidParameterException.class)
- 	public void calculateColumnTotalInvalidTable() {
-	     final Values2D values = null;
+	     assertEquals(23.5, result, 0.000000001d);
+ 	}   
+	
+	//testing that the correct value is returned using negative numbers
+	@Test
+	public void test2ColumnTotal() {
+	//setup for mocking 
+	    Mockery mockingContext = new Mockery();
+	    final Values2D values = mockingContext.mock(Values2D.class);
+	    mockingContext.checking(new Expectations() {
+	         {
+	        	 //creating table using mocked values, looping through arraysr 
+	        	 double[] rows = {-1.0, -1.0, -1.0, -1.0, -1.0};
+		         allowing(values).getColumnCount();
+		         will(returnValue(2));
+		         for(int i = 0; i < rows.length; i++) {
+		           	allowing(values).getValue(i, 0);
+		           	will(returnValue(rows[i]));
+		         }
+		         //setting the amount of rows 
+		         allowing(values).getRowCount();
+		         will(returnValue(5));
+	         }
+	     });
+	     //testing the result we get with the expected result
 	     double result = DataUtilities.calculateColumnTotal(values, 0);
+	     assertEquals(-5.0, result, 0.000000001d);
  	}
-	 
+
+	//testing that the correct value is returned using zero's
+	@Test
+	public void test3ColumnTotal() {
+	//setup for mocking 
+	    Mockery mockingContext = new Mockery();
+	    final Values2D values = mockingContext.mock(Values2D.class);
+	    mockingContext.checking(new Expectations() {
+	         {
+	        	 //creating table using mocked values, looping through arraysr 
+	        	 double[] rows = {0.0, 0.0, 0.0, 0.0, 0.0};
+		         allowing(values).getColumnCount();
+		         will(returnValue(2));
+		         for(int i = 0; i < rows.length; i++) {
+		           	allowing(values).getValue(i, 0);
+		           	will(returnValue(rows[i]));
+		         }
+		         //setting the amount of rows 
+		         allowing(values).getRowCount();
+		         will(returnValue(5));
+	         }
+	     });
+	     //testing the result we get with the expected result
+	     double result = DataUtilities.calculateColumnTotal(values, 0);
+	     assertEquals(0.0, result, 0.000000001d);
+ 	}
+	
+	//Testing that the correct exception is thrown
+ 	@Test (expected = InvalidParameterException.class)
+ 	public void test4ColumnTotal() {
+	     final Values2D values = null;
+	     DataUtilities.calculateColumnTotal(values, 0);
+ 	}
 	 
  	 //TEST DOES NOT WORK BECAUSE OF 
 	 //MOCKING -> Mocking Does not allow us to use this test, 
@@ -79,7 +131,7 @@ public class DataUtilitiesTest {
  
  	//Start of Test Cases for METHOD 2
 	
-	//Using mocking to simulate to a table
+	//testing the row totals when the values are positive numbers
 	@Test
 	public void  test1calculateRowTotal() {
 		
@@ -100,14 +152,14 @@ public class DataUtilitiesTest {
 	    });
 	    //send to function and verify
 	    double result = DataUtilities.calculateRowTotal(values, 0);
-	    assertEquals(result, 9.5, .000000001d);
+	    assertEquals(9.5, result, .000000001d);
 	}
 	
 	//Test to check for correct exception thrown, 
 	@Test (expected = InvalidParameterException.class)
     	public void test2calculateRowTotal() {
         	final Values2D values = null;
-        	double result = DataUtilities.calculateRowTotal(values, 0);
+        	DataUtilities.calculateRowTotal(values, 0);
     	}
 	
 	//	@Test TEST DOES NOT WORK BECAUSE OF MOCKING -> Mocking Does not allow us to use
@@ -119,12 +171,12 @@ public class DataUtilitiesTest {
 	//	    mockingContext.checking(new Expectations() {
 	//	        {
 	//	        
-	//	        	double[] coloumns = {7.5,2.5};
+	//	        	double[] columns = {7.5,2.5};
 	//	            allowing(valuesNew).getColumnCount();
 	//	            will(returnValue(2));
-	//	            for(int i = 0; i < coloumns.length; i++) {
+	//	            for(int i = 0; i < columns.length; i++) {
 	//	            	allowing(valuesNew).getValue(0, i);
-	//	            	will(returnValue(coloumns[i]));
+	//	            	will(returnValue(columns[i]));
 	//	            }
 	//	            allowing(valuesNew).getColumnCount();
 	//	            will(returnValue(2));
@@ -134,47 +186,141 @@ public class DataUtilitiesTest {
 	//	    // verify
 	//	    assertEquals(result, 0, .000000001d);
 	//	}
-	//	
+	
+	//Testing when the values are negative
+	@Test
+	public void test4calculateRowTotal() {
+		Mockery mockingContext = new Mockery();
+		final Values2D values2 = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			//set amount of columns (2 columns, 1 row)
+			one(values2).getColumnCount();
+			will(returnValue(3));
+			//create values within the table (all negative numbers)
+			one(values2).getValue(0, 0);
+			will(returnValue(-1));
+			one(values2).getValue(0, 1);
+			will(returnValue(-3));
+			one(values2).getValue(0, 2);
+			will(returnValue(-4));
+		}
+		});
+		double result = DataUtilities.calculateRowTotal(values2, 0);
+		// verify
+		assertEquals(-8, result, .000000001d);
+		}
+		
+		
+	//Testing when the values are 0
+	@Test
+	public void test5calculateRowTotal() {
+		Mockery mockingContext = new Mockery();
+		final Values2D values2 = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			//set amount of columns (2 columns, 1 row)
+			one(values2).getColumnCount();
+			will(returnValue(2));
+			//create values within the table (all equal to 0)
+			one(values2).getValue(0, 0);
+			will(returnValue(0));
+			one(values2).getValue(0, 1);
+			will(returnValue(0));
+		}
+		});
+		double result = DataUtilities.calculateRowTotal(values2, 0);
+		// verify
+		assertEquals(0, result, .000000001d);
+		}
 	
 	//End of Test Cases for METHOD 2
 
 	// START TEST CASES FOR METHOD 3 - createNumberArray
 	 
-	//testing using valid double values within array
+	//testing that using valid double values returns Number[] array
 	 @Test
-	 public void createNumberArrayValid() {
+	 public void test1CreateNumberArray() {
 	     double[] data = {1.5, 2.5, 3.5, 4.5, 5.5};
 	     java.lang.Number[] result = DataUtilities.createNumberArray(data);
 	     
 	     assertTrue(result instanceof java.lang.Number[]);        
 	 }
 	 
-	 
-	 //testing 
-	 @Test (expected = InvalidParameterException.class)
-	 public void createNumberArrayInvalidData() {
-	     double[] data = null;
+	 //Testing that the method returns a Number[] array returns correct values using positive numbers
+	 @Test
+	 public void test2CreateNumberArray() {
+	     double[] data = {1.5, 2.5, 3.5, 4.5, 5.5};
 	     java.lang.Number[] result = DataUtilities.createNumberArray(data);
+	     
+	     Assert.assertArrayEquals(new Number[]{1.5, 2.5, 3.5, 4.5, 5.5}, result);
 	 }
+	 
+	//Testing that the method returns a Number[] array returns correct values using negative numbers
+	@Test
+	public void test3CreateNumberArray() {
+		double[] data = {-1.0, -1.0, -1.0};
+		java.lang.Number[] result = DataUtilities.createNumberArray(data);
+		     
+		Assert.assertArrayEquals(new Number[]{-1.0, -1.0, -1.0}, result);
+	}
+	
+	//Testing that the method returns a Number[] array returns correct values using zero's
+	@Test
+	public void test4CreateNumberArray() {
+		double[] data = {0.0, 0.0, 0.0};
+		java.lang.Number[] result = DataUtilities.createNumberArray(data);
+			     
+		Assert.assertArrayEquals(new Number[]{0.0, 0.0, 0.0}, result);
+	}
+	
+	//testing that correct exception is thrown
+	@Test (expected = InvalidParameterException.class)
+	public void test5CreateNumberArray() {
+		double[] data = null;
+	    DataUtilities.createNumberArray(data);
+	}
 	 
 	//END of test cases for method 3
 
 	//START of test cases for method 4
-	//Test for number array with double values -> Number Array 
+	//Testing that the method returns a Number[][] array 
 	@Test
-	public void test1PasscreateNumberArray2D(){
+	public void test1CreateNumberArray2D(){
 		double[][] test =  {{1.0,1.0}, {1.0,1.0}};
 		Number[][] result = DataUtilities.createNumberArray2D(test);
 		assertTrue(result instanceof Number[][]);
 	}
 	
+	//Testing that the method returns a Number[][] array returns correct values using positive numbers
+	@Test
+	public void test2CreateNumberArray2D(){
+		double[][] test =  {{1.0,1.0}, {1.0,1.0}};
+		Number[][] result = DataUtilities.createNumberArray2D(test);
+		Assert.assertArrayEquals(new Number[][]{{1.0,1.0}, {1.0,1.0}}, result);
+	}
+	
+	//Testing that the method returns a Number[][] array returns correct values using negative numbers
+	@Test
+	public void test3CreateNumberArray2D(){
+		double[][] test =  {{-1.0,-1.0}, {-1.0, -1.0}};
+		Number[][] result = DataUtilities.createNumberArray2D(test);
+		Assert.assertArrayEquals(new Number[][]{{-1.0, -1.0}, {-1.0, -1.0}}, result);
+	}
+		
+	//Testing that the method returns a Number[][] array returns correct values using zero's
+	@Test
+	public void test4CreateNumberArray2D(){
+		double[][] test =  {{0.0,0.0}, {0.0, 0.0}};
+		Number[][] result = DataUtilities.createNumberArray2D(test);
+		Assert.assertArrayEquals(new Number[][]{{0.0,0.0}, {0.0, 0.0}}, result);
+	}
 	
 	//Test for number array using  NULL 
 	@Test (expected = InvalidParameterException.class)
-	public void test3secondcreateNumberArray2d() {
+	public void test5CreateNumberArray2D() {
 		double[][] test = null;
 		Number[][] result = DataUtilities.createNumberArray2D(test);
 		assertTrue(result instanceof Number[][]);
+		
 	}
 	
 	//END of test cases for METHOD 4
@@ -207,9 +353,36 @@ public class DataUtilitiesTest {
 		//testing using example given in the javadocs, comparing 
 		//key value to the percentage that should be returned
 		KeyedValues result = DataUtilities.getCumulativePercentages(valuesNewKey);
-		assertEquals(result.getValue(0).doubleValue(), 0.3125, .000000001d);
-		assertEquals(result.getValue(1).doubleValue(), 0.875, .000000001d);
-		assertEquals(result.getValue(2).doubleValue(), 1.0, .000000001d);
+		assertEquals(0.3125, result.getValue(0).doubleValue(), .000000001d);
+	}
+	
+	//Test cumulative percentage, using 0 as one of the values
+	@Test
+	public void getCumulativePercentagesTest2() {
+		//before
+		Mockery mockingContextKey = new Mockery();
+		KeyedValues valuesNewKey = mockingContextKey.mock(KeyedValues.class);
+		mockingContextKey.checking(new Expectations() {{
+			//creating a KeyValue object, with two arrays
+			String [] keyArray = {"Key1","Key2","Key3"};
+			double[] values = {0,9,2};
+			//loop through and create object using arrays from before
+			for(int i= 0; i < values.length;i++) {
+				allowing(valuesNewKey).getValue(i);
+				will (returnValue(Double.valueOf(values[i])));
+				allowing(valuesNewKey).getKey(i);
+				will(returnValue(keyArray[i]));
+			}
+			//set item count amount, and Key list values 
+			allowing(valuesNewKey).getItemCount();
+			will(returnValue(3));
+			allowing(valuesNewKey).getKeys();
+			will(returnEnumeration(Arrays.asList(keyArray)));
+		}
+		});
+		KeyedValues result = DataUtilities.getCumulativePercentages(valuesNewKey);
+		assertEquals(0, result.getValue(0).doubleValue(), .000000001d);
+		
 	}
 	
 	//testing percentage using NULL object 
